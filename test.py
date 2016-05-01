e7954d5129450dd98abac27eafc4d83d0ac5aa19
Casey Weed <me@caseyweed.net>
2016-05-01 17:53:47 -0400

    added more detailed sectioned output

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 from som import SOM
 from csv import reader
-from pprint import pprint
 
+# prepare data
 f = open('input.csv', 'r')
 r = reader(f)
 r.next()  # skip header
 data = []
 
+# parse data
 for row in r:
     l = []
     n = row[0]
@@ -17,26 +18,35 @@ for row in r:
         l.append(x)
     data.append(l)
 
+# create SOM & train
 s = SOM()
-
-pprint(s.__dict__)
-
 results = s.train(data)
 
+# restart if only one node (growth threshold not triggered)
 while len(results) == 1:
     print 'Only one node, restarting...'
     s = SOM()
     results = s.train(data)
 
+# print all node values
+print 'All Output Node(s) Associations:'
 for k, v in results.items():
-    print '#', k, '(', len(v),  '):', v
+    print '#{k} ({l}): {v}'.format(k=k, l=len(v), v=', '.join(v))
 
-print 'Good:'
+# print all nodes above threshold
+print '\nGood (contains >={} items):'.format(s.threshold)
 good = {k: v for k, v in results.items() if len(v) >= s.threshold}
 for k, v in good.items():
-    print k, v
+    print '#{k} ({l}): {v}'.format(k=k, l=len(v), v=', '.join(v))
 
-print 'Bad:'
+# print all nodes below threshold
+print '\nBad (contains <{} items):'.format(s.threshold)
 bad = {k: v for k, v in results.items() if len(v) < s.threshold}
 for k, v in bad.items():
-    print k, v
+    print '#{k} ({l}): {v}'.format(k=k, l=len(v), v=', '.join(v))
+
+# print weights of all nodes
+print '\nFinal Weights:'
+for i, node in enumerate(s.outputs):
+    node_string = ['{:.4f}'.format(w) for w in node]
+    print '#{i}: {w}'.format(i=i, w=', '.join(node_string))
